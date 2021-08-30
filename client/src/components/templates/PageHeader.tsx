cf677ff10c162e3379e5989f464b0323cb65e9b8
Jamil Ettel <jamil.ettel@epitech.eu>
2021-08-30 18:22:22 +0200

    :construction: Moved Header logic to header

--- client/src/components/templates/PageHeader.tsx
+++ client/src/components/templates/PageHeader.tsx
@@ -2,10 +2,60 @@ import styles from './PageHeader.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '@/assets/images/logo.png'
+import Button from "@/components/buttons/Button";
+import { RootState, useThunkDispatch } from '@/utils/store';
+import { logout } from '@/modules/auth/authReducer';
+import { connect, ConnectedProps } from 'react-redux';
+import { useEffect, useState } from "react";
+import { getToken } from "@/utils/token";
+import { useRouter } from "next/router";
+import { getUser } from "@/modules/userReducer";
+import User from "@/entities/User";
+import { userDataGetWithExpiry, userDataSetWithExpiry } from "@/utils/localStorageUtils";
 
-export default function PageHeader(props: {
-    children: React.ReactNode
-}) {
+const mapStateToProps = (state: RootState) => {
+    return {
+        auth: {
+            logout: {
+                ...state.auth.logout
+            }
+        },
+        user: { ...state.user },
+    }
+}
+
+const connector = connect(mapStateToProps, { logout, getUser });
+
+function PageHeader(props: ConnectedProps<typeof connector>) {
+    const dispatch = useThunkDispatch();
+    const router = useRouter();
+    const [user, setUser] = useState(null as User | null);
+
+    useEffect(() => {
+        if (getToken() === null)
+            router.push('/login');
+    }, [props.auth.logout.success, props.auth.logout.loaded]);
+
+    useEffect(() => {
+        const user = userDataGetWithExpiry<User>('user');
+        if (user !== null) {
+            setUser(user);
+            return;
+        }
+        const machin = async () => {
+            dispatch(await props.getUser());
+        }
+
+        machin();
+    }, []);
+
+    useEffect(() => {
+        if (props.user.user !== null) {
+            userDataSetWithExpiry('user', props.user.user, 300000);
+            setUser(props.user.user);
+        }
+    }, [props.user.user]);
+
     return (
         <div className={styles.header}>
             <Link href="/">
@@ -18,7 +68,13 @@ export default function PageHeader(props: {
                     </div>
                 </div>
             </Link>
-            {props.children}
+            <div className={styles.spacer} />
+            <Link href="/profile">
+                <div className={styles.username}>{user?.username || ''}</div>
+            </Link>
+            <Button onClick={async () => {dispatch(await props.logout())} }>Logout</Button>
         </div>
     );
 }
+
+export default connector(PageHeader);
--- client/src/modules/homepage/HomePage.tsx
+++ client/src/modules/homepage/HomePage.tsx
@@ -1,71 +1,11 @@
-import Button from "@/components/buttons/Button";
-import { RootState, useThunkDispatch } from '@/utils/store';
-import { logout } from '@/modules/auth/authReducer';
-import { connect, ConnectedProps } from 'react-redux';
-import { useEffect, useState } from "react";
-import { getToken } from "@/utils/token";
-import { useRouter } from "next/router";
-import { getUser } from "@/modules/userReducer";
-import User from "@/entities/User";
-import { userDataGetWithExpiry, userDataSetWithExpiry } from "@/utils/localStorageUtils";
 import PageHeader from "@/components/templates/PageHeader";
 import styles from './HomePage.module.scss';
-import Link from "next/link";
 
-const mapStateToProps = (state: RootState) => {
-    return {
-        auth: {
-            logout: {
-                ...state.auth.logout
-            }
-        },
-        user: { ...state.user },
-    }
-}
-
-const connector = connect(mapStateToProps, { logout, getUser });
-
-function HomePage(props: ConnectedProps<typeof connector>) {
-    const dispatch = useThunkDispatch();
-    const router = useRouter();
-    const [user, setUser] = useState(null as User | null);
-
-    useEffect(() => {
-        if (getToken() === null)
-            router.push('/login');
-    }, [props.auth.logout.success, props.auth.logout.loaded]);
-
-    useEffect(() => {
-        const user = userDataGetWithExpiry<User>('user');
-        if (user !== null) {
-            setUser(user);
-            return;
-        }
-        const machin = async () => {
-            dispatch(await props.getUser());
-        }
-
-        machin();
-    }, []);
-
-    useEffect(() => {
-        if (props.user.user !== null) {
-            userDataSetWithExpiry('user', props.user.user, 300000);
-            setUser(props.user.user);
-        }
-    }, [props.user.user]);
+export default function HomePage() {
 
     return (
         <main>
-            <PageHeader>
-                <div className={styles.spacer} />
-                <Link href="/profile">
-                    <div className={styles.username}>{user?.username || ''}</div>
-                </Link>
-                <Button onClick={async () => {dispatch(await props.logout())} }>Logout</Button>
-            </PageHeader>
+            <PageHeader />
         </main>
     );
 }
-
-export default connector(HomePage);
